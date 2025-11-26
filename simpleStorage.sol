663f2ab1e1981893a085edf82f71864b58d60760
ZerΔch <zerach.sec@gmail.com>
2025-11-26 21:23:35 +0530

    Implement simpleStorage contract with basic functions

---
+++ simpleStorage.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+contract simpleStorage {
+
+    uint  myFavouriteNumber;
+
+    struct person {
+        uint256 favouriteNumber;
+        string name;
+    }
+
+    // uint256 public myArray;
+    person[] public listOfPeople;
+
+    mapping ( string => uint256 ) public nameToFavNumber;
+
+    function store (uint256 _favNumber) public {
+        myFavouriteNumber = _favNumber;
+    }
+
+    function retrieve () public view returns(uint256){
+        return myFavouriteNumber;
+    }
+
+    function addPerson(string memory _name, uint256 _favouriteNumber) public {
+        listOfPeople.push(person(_favouriteNumber,_name));
+        nameToFavNumber[_name] = _favouriteNumber;
+    }
+
+
+}
