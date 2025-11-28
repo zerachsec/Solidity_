245d1516db11db868e3c21d1481bcfaec0f3b973
ZerΔch <zerach.sec@gmail.com>
2025-11-28 21:11:44 +0530

    Add priceConverter library for ETH price conversion

---
+++ priceConverter.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+
+library priceConverter {
+    function getPrice () internal view returns(uint256) {
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x5147eA642CAEF7BD9c1265AadcA78f997AbB9649);
+        (,int256 price,,,) = priceFeed.latestRoundData();
+        return uint256 (price * 1e10 );
+    }
+
+    function getConversionRate(uint256 ethAmount) internal view returns (uint256) {
+        uint256 ethPrice = getPrice();
+        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1e18;
+        return ethAmountInUsd;
+    }
+
+    function getVersion () internal view returns(uint256) {
+        return AggregatorV3Interface(0x5147eA642CAEF7BD9c1265AadcA78f997AbB9649).version();
+    }
+}
