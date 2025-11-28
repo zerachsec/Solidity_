27d04cb7ddc9ecbb13dea3c1b6e8d7d4d594c5e1
ZerΔch <zerach.sec@gmail.com>
2025-11-28 21:46:49 +0530

    Update fundMe.sol

--- fundMe.sol
+++ fundMe.sol
@@ -7,18 +7,35 @@ contract fundMe {
 
     using priceConverter for uint256;
 
-    uint256 public minimumUsd = 5e18;
+    uint256 public constant MINIMUMUSD = 5e18;
     address[] public funders;
     mapping(address funders => uint256 amountFunded) public addressToAmountFunded;
+    address public immutable i_owner;
 
+    constructor(){
+        i_owner = msg.sender;
+    }
 
     function fund () public payable {
-        require (msg.value.getConversionRate() > minimumUsd, "didnt send enough eth");
+        require (msg.value.getConversionRate() > MINIMUMUSD, "didnt send enough eth");
         funders.push(msg.sender);
         addressToAmountFunded[msg.sender] += msg.value;
     }
 
-    function withdraw () public {}
+    function withdraw () public onlyOwner{
+        for (uint256 fundersIndex = 0; fundersIndex < funders.length; fundersIndex ++){
+            address funder = funders[fundersIndex] ;
+            addressToAmountFunded [funder] = 0;
+        }
+        funders = new address[](0);
+        (bool callSucess, ) = payable (msg.sender).call{value : address(this).balance} ("");
+        require(callSucess,"call failed");
+    }
+
+    modifier onlyOwner () {
+        require(msg.sender == i_owner, "sender is not a owner");
+        _;
+    }
     
 
 }
