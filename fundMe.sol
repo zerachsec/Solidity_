0dc4fd42c7e79844572a2c0e19c0e05c04e9a00f
ZerΔch <zerach.sec@gmail.com>
2025-11-28 21:12:01 +0530

    Refactor funding logic to use priceConverter
    Removed price feed functions and integrated priceConverter for conversion rates.

--- fundMe.sol
+++ fundMe.sol
@@ -1,29 +1,24 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+import {priceConverter} from "./priceConverter.sol";
 
 contract fundMe {
 
+    using priceConverter for uint256;
+
     uint256 public minimumUsd = 5e18;
+    address[] public funders;
+    mapping(address funders => uint256 amountFunded) public addressToAmountFunded;
+
 
     function fund () public payable {
-        require (msg.value > minimumUsd, "didnt send enough eth");
-    }
-    function getPrice () public view returns(uint256) {
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x5147eA642CAEF7BD9c1265AadcA78f997AbB9649);
-        (,int256 price,,,) = priceFeed.latestRoundData();
-        return uint256 (price * 1e10 );
+        require (msg.value.getConversionRate() > minimumUsd, "didnt send enough eth");
+        funders.push(msg.sender);
+        addressToAmountFunded[msg.sender] += msg.value;
     }
 
-    function getConversionRate(uint256 ethAmount) public view returns (uint256) {
-        uint256 ethPrice = getPrice();
-        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1e18;
-        return ethAmountInUsd;
-    }
-
-    function getVersion () public view returns(uint256) {
-        return AggregatorV3Interface(0x5147eA642CAEF7BD9c1265AadcA78f997AbB9649).version();
-    }
+    function withdraw () public {}
+    
 
 }
